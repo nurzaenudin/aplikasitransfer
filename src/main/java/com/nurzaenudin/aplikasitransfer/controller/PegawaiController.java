0071245e1ef2f0ac9cea64b0fcf7bde8e7a4b764
Nur Zaenudin <nurzaenudin@kemenkeu.go.id>
2019-01-22 16:54:33 +0700

    fitur tambah pegawai

--- src/main/java/com/nurzaenudin/aplikasitransfer/controller/PegawaiController.java
+++ src/main/java/com/nurzaenudin/aplikasitransfer/controller/PegawaiController.java
@@ -7,9 +7,12 @@ package com.nurzaenudin.aplikasitransfer.controller;
 
 import com.nurzaenudin.aplikasitransfer.DAO.PegawaiDao;
 import com.nurzaenudin.aplikasitransfer.entity.Pegawai;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -30,7 +33,9 @@ public class PegawaiController {
         return pegawaiDao.findAll();
     }
     
-    
-    
-    
+    @PostMapping("")
+    @ResponseBody
+    public void tambahPegawai(@RequestBody @Valid Pegawai pegawai){
+        pegawaiDao.save(pegawai);
+    }
 }
