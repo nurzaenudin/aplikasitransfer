1756c45236b4cf00759b07cf7ee349280c0fa8b0
Nur Zaenudin <nurzaenudin@kemenkeu.go.id>
2019-01-22 11:03:33 +0700

    komit pertama

---
+++ src/main/java/com/nurzaenudin/aplikasitransfer/AplikasitransferApplication.java
@@ -0,0 +1,14 @@
+package com.nurzaenudin.aplikasitransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasitransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasitransferApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/nurzaenudin/aplikasitransfer/AplikasitransferApplicationTests.java
@@ -0,0 +1,17 @@
+package com.nurzaenudin.aplikasitransfer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AplikasitransferApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
