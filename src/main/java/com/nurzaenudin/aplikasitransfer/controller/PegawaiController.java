64c821489b85b8750b71ed0ff01895db7c7df7fc
Nur Zaenudin <nurzaenudin@kemenkeu.go.id>
2019-01-23 14:57:05 +0700

    ubah port ke 8090 dan menambahkan cross origin (CORS)

--- src/main/java/com/nurzaenudin/aplikasitransfer/controller/PegawaiController.java
+++ src/main/java/com/nurzaenudin/aplikasitransfer/controller/PegawaiController.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @author nurzaenudin
  */
 @Controller
+@CrossOrigin(origins="http://localhost:8080")
 @RequestMapping(path="/pegawai")
 public class PegawaiController {
     
