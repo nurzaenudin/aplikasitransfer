f37618c618e5aab0b43c5293869f95791ee9c730
nur zaenudin <nurzaenudin@gmail.com>
2019-03-05 23:08:24 +0700

    authentifikasi melalui user dan password di memory

--- src/main/java/com/nurzaenudin/aplikasitransfer/configuration/WebSecurityConfig.java
+++ src/main/java/com/nurzaenudin/aplikasitransfer/configuration/WebSecurityConfig.java
@@ -5,10 +5,14 @@
  */
 package com.nurzaenudin.aplikasitransfer.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  *
@@ -21,6 +25,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http
+                .formLogin().permitAll()
+                .and().logout()
+                .and().authorizeRequests().anyRequest().authenticated();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    
+    }
+    
+            
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("udin")
+                .authorities("USER_VIEW")
+                .password(passwordEncoder().encode("rahasia"));
+              
+               
+    }
+    
+    
 }
