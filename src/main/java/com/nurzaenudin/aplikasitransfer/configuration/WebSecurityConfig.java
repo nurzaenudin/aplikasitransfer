a85f1fc4c9fe315f275cc121ac825a3356b1ad8b
nur zaenudin <nurzaenudin@gmail.com>
2019-03-08 22:50:28 +0700

    authentication lewat user database. Sudah Berhasil. Next Authorization

--- src/main/java/com/nurzaenudin/aplikasitransfer/configuration/WebSecurityConfig.java
+++ src/main/java/com/nurzaenudin/aplikasitransfer/configuration/WebSecurityConfig.java
@@ -9,10 +9,12 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -43,11 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().authorizeRequests().anyRequest().authenticated();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    
-    }
+
     
             
 //    @Override
@@ -64,16 +62,39 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private DataSource datasource;
     
-    @Override
+    @Override    
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .jdbcAuthentication()
                 .dataSource(datasource)
+                .passwordEncoder(passwordEncoder())
                 .usersByUsernameQuery(SQL_LOGIN)
-                .authoritiesByUsernameQuery(SQL_PERMISSION)
-                .passwordEncoder(passwordEncoder());
+                .authoritiesByUsernameQuery(SQL_PERMISSION);
     }
     
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    
+    }
+
+    
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    @Bean
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+    
+
     
     
     
